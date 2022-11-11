3d5b25c74bd402eaf47405933a3bd6b3dad833ef
sankairuz <sankairuz@gmail.com>
2022-11-11 11:56:19 +0300

    Редактирование по pep8

--- 2/2_3_2.py
+++ 2/2_3_2.py
@@ -1,9 +1,13 @@
+import math
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time, math
+
 
 def calculate(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 try:
     link = 'http://suninjuly.github.io/redirect_accept.html'
@@ -17,13 +21,6 @@ try:
     x = browser.find_element(By.ID, 'input_value').text
     browser.find_element(By.NAME, 'text').send_keys(calculate(x))
     browser.find_element(By.CLASS_NAME, 'btn').click()
-
-
-
-
-
-
-
 
 
 finally:
