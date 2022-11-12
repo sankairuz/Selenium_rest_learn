1a9884b58ad786887d63328bd46dae662c20c2f2
sankairuz <sankairuz@gmail.com>
2022-11-12 11:44:12 +0300

    Внутри класс с двумя функциями/тестами

--- Step3/3_2_unit.py
+++ Step3/3_2_unit.py
@@ -4,7 +4,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-3
+# объявляем класс, содержащий функции
 class TestAbs(unittest.TestCase):
     def test1(self):
         try:
